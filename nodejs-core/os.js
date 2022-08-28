e36a48a57db889868273f9bcf9a1a119bd9e1c60
Muhammad <muhdibee@gmail.com>
2022-08-28 12:56:09 +0100

    Getting user Information

--- nodejs-core/os.js
+++ nodejs-core/os.js
@@ -1,8 +1,11 @@
 const os = require('os');
 
-//Get the current user information
+/*Get the current user information
+** os.userInfo() returns an Object. */
 const user = os.userInfo();
-console.log(`User Information: ${user}`);
+console.log(`User Information: ${JSON.stringify(user)}`);
 
+/* Geting the system architecture.
+** Return value is equivalent to process.arch */
 const sysArch = os.arch();
 console.log(`System Architecture: ${sysArch}`);
