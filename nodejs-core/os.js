b9512395ac8ee2387172bd5cf08241a5d8447f53
Muhammad <muhdibee@gmail.com>
2022-08-28 12:33:34 +0100

    Getting os cpu architecture

--- nodejs-core/os.js
+++ nodejs-core/os.js
@@ -1,1 +1,8 @@
 const os = require('os');
+
+//Get the current user information
+const user = os.userInfo();
+console.log(`User Information: ${user}`);
+
+const sysArch = os.arch();
+console.log(`System Architecture: ${sysArch}`);
