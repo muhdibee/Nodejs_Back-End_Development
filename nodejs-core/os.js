3a58477fc0bdfb88234885ea91137116206c6283
Muhammad <muhdibee@gmail.com>
2022-08-28 17:23:53 +0100

    Geting the amount of CPUs on a machine

--- nodejs-core/os.js
+++ nodejs-core/os.js
@@ -33,4 +33,7 @@ console.log(`FreeMemory: ${freeMemory} B`);
 
 // Geting information about all system cpus. Returns an array of objects containing information about each CPU.
 const cpus = os.cpus();
-console.log(`cpus: ${JSON.stringify(cpus)} B`);
+console.log(`cpus: ${JSON.stringify(cpus)}`);
+
+// Geting the number of CPUs on a machine (CPU cores)
+console.log(`cpus: ${cpus.length} Cores`);
