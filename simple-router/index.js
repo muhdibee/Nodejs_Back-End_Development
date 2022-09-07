544e2ffd73fb8512e4c8e7f33006763b9d72c2bf
Muhammad <muhdibee@gmail.com>
2022-09-07 18:33:08 +0100

    handling simple GET request

---
+++ simple-router/index.js
@@ -0,0 +1,35 @@
+//Require the server.
+const http = require('http');
+
+// Requiring fixture module
+const {books, authors} = require('./fixtures');
+
+const HOSTNAME = 'localhost';
+const PORT = 3000;
+
+//Create a server.
+const SERVER = http.createServer((req, res) => {
+    switch(req.url){
+        case '/':
+            res.writeHead(200)
+            res.end('You are welcome.');
+            break;
+
+        case '/books':
+            res.writeHead(200)
+            res.end('Requesting for books.');
+            break;
+        case '/authors':
+            res.writeHead(200)
+            res.end('requesting for authors.');
+            break;
+        default:
+            res.writeHead(400)
+            res.end('Request not found');
+    }
+});
+
+//Listen on specific port and host name.
+SERVER.listen(PORT, HOSTNAME, () => {
+    console.log(`Server started successfully at https://${HOSTNAME}:${PORT}`);
+});
