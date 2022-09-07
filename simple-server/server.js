2e4ae929156be93e464d213c78d40b615267dba6
Muhammad <muhdibee@gmail.com>
2022-09-07 17:43:53 +0100

    Added: fixture.js data

--- simple-server/server.js
+++ simple-server/server.js
@@ -4,6 +4,7 @@ const http = require('http');
 const HOSTNAME = 'localhost';
 const PORT = 3000;
 
+//Create a server.
 const SERVER = http.createServer((req, res) => {
     // console.log(req);
     res.writeHead(200);
@@ -13,6 +14,7 @@ const SERVER = http.createServer((req, res) => {
 
 });
 
+//Listen on specific port and host name.
 SERVER.listen(PORT, HOSTNAME, () => {
-    console.log(`Server started successfully at https://${HOSTNAME}:${PORT}`)
-})
+    console.log(`Server started successfully at https://${HOSTNAME}:${PORT}`);
+});
