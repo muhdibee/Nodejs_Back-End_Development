d3a78f8dc22fda11766bf0d8ab5eac5ec9ccba02
Muhammad <muhdibee@gmail.com>
2022-09-07 15:34:37 +0100

    Simple server responses

--- simple-server/server.js
+++ simple-server/server.js
@@ -2,9 +2,16 @@
 const http = require('http');
 
 const HOSTNAME = 'localhost';
-const PORT = 8000;
+const PORT = 3000;
 
-const SERVER = http.createServer();
+const SERVER = http.createServer((req, res) => {
+    // console.log(req);
+    res.writeHead(200);
+    res.write('Hello from the server.');
+    res.write('\nHello again');
+    res.end('\nThe end.');
+
+});
 
 SERVER.listen(PORT, HOSTNAME, () => {
     console.log(`Server started successfully at https://${HOSTNAME}:${PORT}`)
