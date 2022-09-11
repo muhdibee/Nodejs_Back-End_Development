cb4d6daae01919c3fd2754ab635532cae849ce28
Muhammad <muhdibee@gmail.com>
2022-09-11 05:12:35 +0100

    Getting all books using /books

--- server-method/index.js
+++ server-method/index.js
@@ -1,14 +1,46 @@
-//Require the server.
 const http = require('http');
-const nodemon = require('nodemon');
+const path = require('path');
+const fs = require('fs');
 
-const HOSTNAME = '0.0.0.0';
+const bookDbPath = path.join(__dirname, 'db', 'books.json');
+
+const HOST_NAME = '0.0.0.0';
 const PORT = 3000;
 
 //Create a server.
-const SERVER = http.createServer((req, res) => {});
+const SERVER = http.createServer((req, res) => {
+    if (req.url === '/books' && req.method === 'GET'){
+        getAllBooks(req, res);
+     }
+     /*else if (req.url === '/books' && req.method === 'POST'){
+    //     addBook(req, res);
+    // }else if (req.url === '/books' && req.method === 'PUT'){
+    //     UpdateBook(req, res);
+    // }else if (req.url === '/books' && req.method === 'DELET'){
+    //     deleteBook(req, res);
+    // } */
+
+});
+
+// Retreive all books using /books
+const getAllBooks = function (req, res){
+    fs.readFile(bookDbPath, 'utf-8', async(err, data) => {
+        if(err){
+            console.log(err)
+            res.writeHead(500, 'Error in reading bookDb path');
+            res.end('An error occured');
+        }else {
+            res.end(data);
+            console.log('success: /Books GET request');
+        }
+    })
+}
+
+
+
+
 
 //Listen on specific port and host name.
-SERVER.listen(PORT, HOSTNAME, () => {
-    console.log(`Server started successfully at https://${HOSTNAME}:${PORT}`);
+SERVER.listen(PORT, HOST_NAME, () => {
+    console.log(`Server started successfully at https://${HOST_NAME}:${PORT}`);
 });
