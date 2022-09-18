d5f04f4ad3f62f0af9eacaa2824c4c0ffd3bfc06
Muhammad <muhdibee@gmail.com>
2022-09-19 00:19:15 +0100

    Tested out user.js module

---
+++ test-driven-development/src/user.js
@@ -0,0 +1,12 @@
+const getUserDetails = () => {
+	return {
+		firstname: "Muhammad",
+		lastname: "Ibrahim",
+		age: 25,
+		height: 167,
+	};
+};
+
+module.exports = {
+	getUserDetails,
+};
--- test-driven-development/test/user.test.js
+++ test-driven-development/test/user.test.js
@@ -4,5 +4,7 @@ test("User Obj is returned properly", () => {
 	const userObj = user.getUserDetails();
 	expect(userObj.firstname).toBe("Muhammad");
 	expect(userObj.lastname).toBe("Ibrahim");
-	expect(userObj.age).toBe(24);
+	expect(userObj.age).toBe(25);
+	expect(userObj.height).toBeGreaterThan(150);
+	expect(userObj).toHaveProperty("age");
 });
