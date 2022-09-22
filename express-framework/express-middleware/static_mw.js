dd4874086493b77de538528e1bea98a5af8aedbb
Muhammad <muhdibee@gmail.com>
2022-09-22 17:52:25 +0100

    Getting request body using Body-parser middleware

--- express-framework/express-middleware/static_mw.js
+++ express-framework/express-middleware/static_mw.js
@@ -7,6 +7,14 @@ const PORT = 3000;
 // Add a static middleware to serve static files from the public directory.
 app.use(express.static("public"));
 
+// Add third party middleware to parse the request body
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+	res.status(200);
+	res.send("Hello World");
+});
+
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
