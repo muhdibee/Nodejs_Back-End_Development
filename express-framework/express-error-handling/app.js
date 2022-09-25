11ab819f3011c8ccb90cf46f5028905d869f2477
Muhammad <muhdibee@gmail.com>
2022-09-25 14:31:56 +0100

    Handling async errors in express.

--- express-framework/express-error-handling/app.js
+++ express-framework/express-error-handling/app.js
@@ -10,15 +10,61 @@ app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(logger("dev"));
 
-// Synchronous error is automatically handled by express
+// Synchronous error is automatically handled by express.
 app.get("/", (req, res) => {
 	throw new Error("Hello error!");
 });
 
 // Asynchronous error is not handled by express.
-app.get("/file", async (req, res) => {
-	const file = await fsPromises.readFile("./no-such-file.txt");
-	res.sendFile(file);
+app.get("/file", async (req, res, next) => {
+	try {
+		const file = await fsPromises.readFile("./no-such-file.txt");
+		res.sendFile(file);
+	} catch (error) {
+		error.type = "Not Found";
+		next(error);
+	}
+});
+
+app.get("/text", async (req, res, next) => {
+	try {
+		const file = await fsPromises.readFile("./no-such-file.txt");
+		res.sendFile(file);
+	} catch (error) {
+		error.type = "Not Found";
+		next(error);
+	}
+});
+
+app.get("/user", async (req, res, next) => {
+	try {
+		const file = await fsPromises.readFile("./no-such-file.txt");
+		res.sendFile(file);
+	} catch (error) {
+		error.type = "Redirect";
+		next(error);
+	}
+});
+
+// Handle asynchronous error using error middleware.
+app.use((error, req, res, next) => {
+	console.log("Error Handling Middleware called");
+	console.log("Path: ", req.path);
+	console.error("Error: ", error);
+
+	if (error.type == "Redirect") res.redirect("error.html");
+	else if (error.type == "Not Found")
+		// arbitrary condition check
+		res.status(404).send({
+			errno: error.errno,
+			code: error.code,
+			type: error.type,
+		});
+	else {
+		res.status(500).send(error);
+	}
+
+	next(); // next is required to call next middleware if any.
 });
 
 app.listen(PORT, () => {
