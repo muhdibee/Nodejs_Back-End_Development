056ce469dae1f15791f944f0b6de6eb9091fe585
Muhammad <muhdibee@gmail.com>
2022-09-25 13:48:59 +0100

    Async errors not handled by express.

--- express-framework/express-error-handling/app.js
+++ express-framework/express-error-handling/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fsPromises = require("fs").promises;
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 
@@ -8,6 +9,17 @@ const PORT = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(logger("dev"));
+
+// Synchronous error is automatically handled by express
+app.get("/", (req, res) => {
+	throw new Error("Hello error!");
+});
+
+// Asynchronous error is not handled by express.
+app.get("/file", async (req, res) => {
+	const file = await fsPromises.readFile("./no-such-file.txt");
+	res.sendFile(file);
+});
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
