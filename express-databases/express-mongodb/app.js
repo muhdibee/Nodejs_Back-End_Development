3509e69e81ea420647f774d3e943a2769c49185d
Muhammad <muhdibee@gmail.com>
2022-09-29 18:13:20 +0100

    Connect to mongodb atlas.

--- express-databases/express-mongodb/app.js
+++ express-databases/express-mongodb/app.js
@@ -1,3 +1,19 @@
 const express = require("express");
+const logger = require("morgan");
+require("dotenv").config();
+const { connectionToMongoDB } = require("./db");
 
+const app = express();
 const PORT = process.env.PORT;
+
+// Connecting to mongodb instance.
+connectionToMongoDB();
+
+app.use(logger("dev"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.status(200).send("Welcome Home.");
+});
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}.`));
---
+++ express-databases/express-mongodb/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const MONGODB_CONNECTION_URL = process.env.MONGODB_CONNECTION_URL;
+
+const connectionToMongoDB = () => {
+	mongoose.connect(MONGODB_CONNECTION_URL);
+
+	mongoose.connection.on("connected", () => {
+		console.log("Connected successfully to mongoDB");
+	});
+	mongoose.connection.on("error", (error) => {
+		console.log("Error: ", error);
+		console.log("An error ocurred");
+	});
+};
+
+module.exports = { connectionToMongoDB };
